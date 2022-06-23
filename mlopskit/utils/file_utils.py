83bdb42efe2c06f782606932688bb5f8cff20e1a
leepand <leepand@leepanddeMacBook-Pro.local>
2022-06-23 18:33:33 +0800

    add file_utils.py

---
+++ mlopskit/utils/file_utils.py
@@ -0,0 +1,43 @@
+from urllib.parse import unquote
+from urllib.request import pathname2url
+import posixpath
+import os
+
+import tarfile
+import gzip
+import tempfile
+
+
+def relative_path_to_artifact_path(path):
+    if os.path == posixpath:
+        return path
+    if os.path.abspath(path) == path:
+        raise Exception("This method only works with relative paths.")
+    return unquote(pathname2url(path))
+
+
+
+
+def make_tarfile(output_filename, source_dir, archive_name, custom_filter=None):
+    # Helper for filtering out modification timestamps
+    def _filter_timestamps(tar_info):
+        tar_info.mtime = 0
+        return tar_info if custom_filter is None else custom_filter(tar_info)
+
+    unzipped_file_handle, unzipped_filename = tempfile.mkstemp()
+    try:
+        with tarfile.open(unzipped_filename, "w") as tar:
+            tar.add(source_dir, arcname=archive_name, filter=_filter_timestamps)
+        # When gzipping the tar, don't include the tar's filename or modification time in the
+        # zipped archive (see https://docs.python.org/3/library/gzip.html#gzip.GzipFile)
+        with gzip.GzipFile(
+            filename="", fileobj=open(output_filename, "wb"), mode="wb", mtime=0
+        ) as gzipped_tar, open(unzipped_filename, "rb") as tar:
+            gzipped_tar.write(tar.read())
+    finally:
+        os.close(unzipped_file_handle)
+        
+        
+#make_tarfile(
+#        output_filename='dd.tar.gz', source_dir='0', archive_name="some-archive"
+#    )
