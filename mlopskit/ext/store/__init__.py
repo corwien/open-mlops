dd3d5a6c08bbe37ee085a1e5bd14203c4166807d
leepand <leepand@leepanddeMacBook-Pro.local>
2022-06-25 21:44:25 +0800

    uodate pickle storage

--- mlopskit/ext/store/__init__.py
+++ mlopskit/ext/store/__init__.py
@@ -1,2 +1,13 @@
 """用于存储和加载mlopskit用到的数据、元数据和工件
 """
+
+from mlopskit.ext.store.pickle.pickle_storage import PickleDataSet
+from mlopskit.ext.store.api.api_dataset import APIDataSet
+from mlopskit.ext.store.yaml.yaml_dataset import YAMLDataSet
+
+
+__all__ = [
+    'PickleDataSet',
+    'YAMLDataSet',
+    'APIDataSet'
+]
