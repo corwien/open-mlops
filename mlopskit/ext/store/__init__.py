c3dbc8a62af7b9dd7fd99ec455890db3f6aafc4f
leepand <leepand@leepanddeMacBook-Pro.local>
2022-06-25 22:28:08 +0800

    uodate redis storage

--- mlopskit/ext/store/__init__.py
+++ mlopskit/ext/store/__init__.py
@@ -4,10 +4,12 @@
 from mlopskit.ext.store.pickle.pickle_storage import PickleDataSet
 from mlopskit.ext.store.api.api_dataset import APIDataSet
 from mlopskit.ext.store.yaml.yaml_dataset import YAMLDataSet
+from mlopskit.ext.store.redis.redis_dataset import RedisDataSet
 
 
 __all__ = [
     'PickleDataSet',
     'YAMLDataSet',
-    'APIDataSet'
+    'APIDataSet',
+    'RedisDataSet'
 ]
