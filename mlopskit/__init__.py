353c0dfe6db63313c50c1ee56095bcfe9dd56914
leepand <leepand@leepanddeMacBook-Pro.local>
2022-06-26 12:33:35 +0800

    init

--- mlopskit/__init__.py
+++ mlopskit/__init__.py
@@ -1,1 +1,11 @@
-__version__='0.0.1'
+import warnings
+
+from mlopskit.core.library import ModelLibrary, load_model  # NOQA
+from mlopskit.core.model import Model  # NOQA
+
+# Silence Tensorflow warnings
+# https://github.com/tensorflow/tensorflow/issues/30427
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+__version__ = "0.0.1"
